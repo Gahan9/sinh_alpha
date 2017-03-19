25be1e7ab2b992c7bfda272472147fd3b326b77d
Gahan9 <gahan@quixom.com>
2017-03-20 00:01:08 +0530

    updated to take config file only dynamically

--- sinh/core/config.py
+++ sinh/core/config.py
@@ -1,11 +1,18 @@
-﻿import ConfigParser, os
+﻿from __future__ import print_function
+import ConfigParser, os
 
 def config():
+    """Parsing the Config file stored in root directory"""
     cfg = ConfigParser.ConfigParser()
-    for f in ('sinh.cfg', '/home/jarvis/honeypot/sinh/sinh.cfg', 'honeypot/sinh/sinh.cfg'):
+    # get_path = lambda x: os.path.abspath(os.path.join(os.path.dirname(__file__), x))
+    # DATA_DIR = get_path('sinh.cfg')
+    # for files in os.listdir(DATA_DIR):
+    #     f_p = open(os.path.join(DATA_DIR, files))
+
+    # for f in f_p:
+    for f in ('sinh.cfg', 'sinh/sinh.cfg'):
         if os.path.exists(f):
             cfg.read(f)
             return cfg
     return None
 
-# vim: set sw=4 et:
