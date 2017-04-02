6427eb8c9f98d14978244d9420b35831c1508bf4
Gahan9 <gahan@quixom.com>
2017-04-02 13:15:44 +0530

    updated urls for class view of input page

--- honeyweb/honey/urls.py
+++ honeyweb/honey/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^password_change', views.change_password, name="password_change"),
-    url(r'^input$', views.input_page, name='input_page'),
+    url(r'^input$', views.InputPageView.as_view(), name='input_page'),
     url(r'^auth$', views.auth_page, name='auth_page'),
     url(r'^client$', views.client_page, name='client_page'),
     url(r'^downloads$', views.downloads_page, name='downloads_page'),
