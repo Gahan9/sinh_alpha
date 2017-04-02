5cce44b9cc8f32622959b300c73815226c9af6bc
Gahan9 <gahan@quixom.com>
2017-04-02 15:56:55 +0530

    Optimized code using Class Based view and deleted all unnecessary html along with it

--- honeyweb/honey/urls.py
+++ honeyweb/honey/urls.py
@@ -4,12 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^password_change', views.change_password, name="password_change"),
     url(r'^input$', views.InputPageView.as_view(), name='input_page'),
-    url(r'^auth$', views.auth_page, name='auth_page'),
-    url(r'^client$', views.client_page, name='client_page'),
-    url(r'^downloads$', views.downloads_page, name='downloads_page'),
-    url(r'^session$', views.session_page, name='session_page'),
-    url(r'^sensor$', views.sensor_page, name='sensor_page'),
-    url(r'^sortedRequest', views.sortedRequest, name='sorted')
+    url(r'^auth$', views.AuthPageView.as_view(), name='auth_page'),
+    url(r'^client$', views.ClientPageView.as_view(), name='client_page'),
+    url(r'^downloads$', views.DownloadsPageView.as_view(), name='downloads_page'),
+    url(r'^session$', views.SessionPageView.as_view(), name='session_page'),
+    url(r'^sensor$', views.SensorPageView.as_view(), name='sensor_page'),
 ]
--- honeyweb/honey/views.py
+++ honeyweb/honey/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404, request, Js
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView
 from django.views.generic import TemplateView
 from django_tables2 import MultiTableMixin
@@ -25,81 +26,63 @@ class InputPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
     """ Show all input commands entered by attacker """
     login_url = reverse_lazy('login')
     table_class = InputTable
-    template_name = 'input_page.html'
+    template_name = 'table_show.html'
     tables = [InputTable(Input.objects.all())]
     table_pagination = {
         'per_page': 15
     }
 
 
-def sortedRequest(request):
-    """ handle ajax request for sorting """
-    if request.is_ajax():
-        field_to_sort = request.GET.get('field')
-        sortedResponse = Auth.objects.all().order_by('-'+field_to_sort)
-        return JsonResponse(sortedResponse)
-    else:
-        JsonResponse("Error!")
+class AuthPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
+    """ Show all input commands entered by attacker """
+    login_url = reverse_lazy('login')
+    table_class = AuthTable
+    template_name = 'table_show.html'
+    tables = [AuthTable(Auth.objects.all())]
+    table_pagination = {
+        'per_page': 15
+    }
 
 
-@login_required(login_url="login/")
-def auth_page(request):
-
-    auth_data = Auth.objects.all().order_by('-timestamp')
-    template = "auth_page.html"
-    context = {"auth_data": auth_data}
-    return render(request, template, context)
-
-
-@login_required(login_url="login/")
-def client_page(request):
-    client_data = Clients.objects.all()
-    template = "client_page.html"
-    context = {"client_data": client_data}
-    return render(request, template, context)
+class ClientPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
+    """ Show all input commands entered by attacker """
+    login_url = reverse_lazy('login')
+    table_class = Clients
+    template_name = 'table_show.html'
+    tables = [ClientsTable(Clients.objects.all())]
+    table_pagination = {
+        'per_page': 15
+    }
 
 
-@login_required(login_url="login/")
-def downloads_page(request):
-    downloads_data = Downloads.objects.all()
-    template = "downloads_page.html"
-    context = {"downloads_data": downloads_data}
-    return render(request, template, context)
+class DownloadsPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
+    """ Show all input commands entered by attacker """
+    login_url = reverse_lazy('login')
+    table_class = Downloads
+    template_name = 'table_show.html'
+    tables = [DownloadsTable(Downloads.objects.all())]
+    table_pagination = {
+        'per_page': 15
+    }
 
 
-@login_required(login_url="login/")
-def session_page(request):
-    session_data = Sessions.objects.all()
-    template = "session_page.html"
-    context = {"session_data": session_data}
-    return render(request, template, context)
+class SessionPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
+    """ Show all input commands entered by attacker """
+    login_url = reverse_lazy('login')
+    table_class = Sessions
+    template_name = 'table_show.html'
+    tables = [SessionsTable(Sessions.objects.all())]
+    table_pagination = {
+        'per_page': 15
+    }
 
 
-@login_required(login_url="login/")
-def sensor_page(request):
-    sensor_data = Sensors.objects.all()
-    template = "sensor_page.html"
-    context = {"sensor_data": sensor_data}
-    return render(request, template, context)
-
-
-def search_auth(request):
-    """ Implemented to sniff entries in database """
-    auth_filter = {"session": request.GET.get('session', None),
-          "success": request.GET.get('success', None),
-          "username": request.GET.get('username', None),
-          "password": request.GET.get('password', None),
-           "timestamp": request.GET.get('timestamp', None),
-           "ip": request.GET.get('ip', None)
-          }
-
-    req_data = dict(filter(lambda x: x[1], auth_filter.items()))
-
-    id_ = request.GET.get('id', None)
-
-    if req_data:
-        refined_search = Auth.objects.filter(**req_data)
-        return render(request, 'results.html', {'refined_search': refined_search}, {'query': id_})
-    else:
-        error = 'No match found'
-        return HttpResponse(error)
+class SensorPageView(LoginRequiredMixin, MultiTableMixin, TemplateView):
+    """ Show all input commands entered by attacker """
+    login_url = reverse_lazy('login')
+    table_class = Sensors
+    template_name = 'table_show.html'
+    tables = [SensorsTable(Sensors.objects.all())]
+    table_pagination = {
+        'per_page': 15
+    }
